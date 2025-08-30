f66529cef8acc20a7a04aeacf985fa78d2995e57
Anirban Das <anirban140963@gmail.com>
2025-08-30 21:06:11 +0530

    first commit

---
+++ vite/02addFile/src/AddFile.jsx
@@ -0,0 +1,7 @@
+function AddFile() {
+    return(
+     <h1>add new file</h1>   
+    );
+}
+
+export default AddFile
---
+++ vite/02addFile/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import AddFile from './AddFile.js'
+
+function App() {
+  
+
+  return (
+    <>
+      <AddFile />
+    </>
+  );
+}
+
+export default App
